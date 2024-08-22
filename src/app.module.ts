b8248c9d1efc5e0291ada425c83980441468e038
linzheng <alidlg9045@126.com>
2024-08-22 17:00:09 +0800

    使用 migration 做数据迁移

--- src/app.module.ts
+++ src/app.module.ts
@@ -31,7 +31,7 @@ import * as path from 'path';
           username: configService.get('mysql_server_username'),
           password: configService.get('mysql_server_password'),
           database: configService.get('mysql_server_database'),
-          synchronize: true,
+          synchronize: false,
           logging: true,
           entities: [User, Role, Permission, MeetingRoom, Booking],
           connectorPackage: 'mysql2',
